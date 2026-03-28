92820e088620f930258e045f03fda947778041ad
Perry Dev <perrydev17@gmail.com>
2026-03-28 15:23:43 -0400

    add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-function App() {
+import Button from "./components/Button";
 
+
+function App() {
   return (
-    <>
-    
-    </>
+    <main className="mx-auto px-4 py-4">
+      <div className="space-x-2">
+      <Button>Default</Button>
+      <Button variant="primary">Submit</Button>
+      <Button variant="destructive">Cancel</Button>
+      <Button variant="warning">Warning</Button>
+      </div>
+    </main>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,49 @@
+import { useRender, mergeProps } from "@base-ui/react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { twMerge } from "tailwind-merge";
+
+const buttonVariants = cva(
+  "inline-flex justify-center items-center cursor-pointer hover:opacity-90 focus-visible:ring-foreground focus-visib e:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none border-3 border-foreground border-background shadow-raised bg-linear-to-tl disabled:opacity-50 disabled:pointer-events-none font-medium active:shadow-inset transition-all duration-250 east-out active:scale-97", {
+  variants: {
+    variant: {
+      default: "from-background to-highlight/75",
+      primary: "text-primary from-primary/5 to-primary/25",
+      destructive: "text-destructive from-primary/5 to-primary/25",
+      warning: "text-warning from-primary/5 to-primary/25"
+    },
+    size: {
+      default:"h-12 px-4",
+      icon: "h-12 w-12",
+    },
+    shape: {
+      default: "rounded-xl",
+      circle: "rounded-full"
+    }
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+    shape: "default"
+  }
+});
+
+interface ButtonProps extends useRender.ComponentProps<"button">, VariantProps<typeof buttonVariants> {}
+
+export default function Button(props: any) {
+  const { render, ...rest } = props;
+  const mergedProps = mergeProps(props, {
+    className: twMerge(buttonVariants({
+      variant: props.variant, 
+      size: props.size, 
+      shape: props.shape
+    }), props.className
+  )});
+
+  const element = useRender({
+    defaultTagName: 'button',
+    render: props.render,
+    props: mergedProps,
+  });
+
+  return element;
+}
